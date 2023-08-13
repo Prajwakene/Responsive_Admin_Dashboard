5c29dea96473d7e015a5019a69e95f84f7d08dc7
Prajwal Kene <127944958+Prajwakene@users.noreply.github.com>
2023-08-13 20:37:17 +0530

    ResponsiveAdminDashboard

---
+++ order.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "Folable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Refunded",
+    shipping: "Success",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "25867",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49812",
+    paymentStatus: "Declined",
+    shipping: "Due",
+  },
+  {
+    productName: "Drone With Camera Drone",
+    productNumber: "78549",
+    paymentStatus: "success",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Mini Drone",
+    productNumber: "24579",
+    paymentStatus: "success",
+    shipping: "Delivered",
+  },
+];
